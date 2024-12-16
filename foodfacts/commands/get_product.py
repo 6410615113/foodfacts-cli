fc1965963c4f60cb091df5e8dc7c90a63a89edfa
Pitchakan Leephud <6410615113@cn103>
2024-12-16 17:33:29 +0700

    Add 'get_product' function

---
+++ foodfacts/commands/get_product.py
@@ -0,0 +1,31 @@
+import requests
+from tabulate import tabulate
+
+def register_command(subparsers):
+    get_product_parser = subparsers.add_parser('get_product')
+    get_product_parser.add_argument('category')
+    get_product_parser.set_defaults(func=execute)
+
+def execute(args):
+    category = args.category
+    url = f"https://world.openfoodfacts.org/cgi/search.pl?search_terms={category}&action=process&json=true"
+    
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        products = data.get('products', [])
+        
+        if products:
+            table_data = []
+            for product in products:
+                product_name = product.get('product_name', 'N/A')
+                product_id = product.get('code', 'N/A')
+                table_data.append([product_name, product_id])
+
+            headers = ["Product Name", "Product ID"]
+            print(tabulate(table_data, headers=headers, tablefmt="grid"))
+        else:
+            print(f"No products found for category '{category}'.")
+    else:
+        print(f"Failed to fetch products for category '{category}'. HTTP Status Code: {response.status_code}")
